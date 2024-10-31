353e07e620b7fd1cad399c9a0fba05e3639cfbc3
nikrow <nicolasjosea@gmail.com>
2024-10-31 00:02:40 -0300

    bet 2.4.7.2

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,15 +1,1 @@
 import './bootstrap';
-
-import Alpine from 'alpinejs';
-import collapse from '@alpinejs/collapse';
-import focus from '@alpinejs/focus';
-
-window.Alpine = Alpine;
-
-Alpine.plugin(collapse);
-Alpine.plugin(focus);
-
-Alpine.start();
-
-import '../../vendor/filament/filament/resources/js/filament.js';
-import '../../vendor/filament/widgets/resources/js/widgets.js';
