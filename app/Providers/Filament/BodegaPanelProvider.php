05a45861e13183a51d5c9d3fc0aaa40806a9e8e8
nikrow <nicolasjosea@gmail.com>
2024-11-04 09:12:06 -0300

    bet 2.5.0

--- app/Providers/Filament/BodegaPanelProvider.php
+++ app/Providers/Filament/BodegaPanelProvider.php
@@ -2,17 +2,15 @@
 
 namespace App\Providers\Filament;
 
-use App\Filament\Pages\Tenancy\RegisterField;
+
 use App\Filament\Resources\ActivityLogResource;
 use App\Models\Field;
-use BezhanSalleh\FilamentShield\FilamentShieldPlugin;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
 use Filament\Pages;
 use Filament\Panel;
 use Filament\PanelProvider;
-use Filament\Support\Colors\Color;
 use Filament\Widgets;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
@@ -31,7 +29,7 @@ class BodegaPanelProvider extends PanelProvider
         return $panel
             ->default()
             ->id('campo')
-            ->path('/')
+            ->path('')
             ->login()
             ->colors([
                 'primary' => ('#568203'),
---
+++ resources/views/vendor/filament/components/brand.blade.php
@@ -0,0 +1,1 @@
+<img src="{{ asset('public/img/logovector2.svg') }}" alt="Logo" style="height: 1.5rem;" class="h-10">
