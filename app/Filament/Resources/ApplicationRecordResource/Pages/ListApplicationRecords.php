2ae33b0bc58c33f4b97ba4144e9f337425e23e75
nikrow <nicolasjosea@gmail.com>
2024-11-18 21:09:58 -0300

    bet 2.7.1

---
+++ app/Filament/Resources/ApplicationRecordResource/Pages/ListApplicationRecords.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Resources\ApplicationRecordResource\Pages;
+
+use App\Filament\Resources\ApplicationRecordResource;
+use Filament\Actions;
+use Filament\Resources\Pages\ListRecords;
+
+class ListApplicationRecords extends ListRecords
+{
+    protected static string $resource = ApplicationRecordResource::class;
+
+
+}
