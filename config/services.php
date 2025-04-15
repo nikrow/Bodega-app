59e2207ada062921cd0f2e1ecd069b3cdfc744d0
nikrow <nicolasjosea@gmail.com>
2025-04-15 15:46:22 -0400

    test3

--- config/services.php
+++ config/services.php
@@ -30,8 +30,7 @@ return [
 
     'slack' => [
         'notifications' => [
-            'bot_user_oauth_token' => env('SLACK_BOT_USER_OAUTH_TOKEN'),
-            'channel' => env('SLACK_BOT_USER_DEFAULT_CHANNEL'),
+            'webhook_url' => env('SLACK_WEBHOOK_URL'),
         ],
     ],
 ];
