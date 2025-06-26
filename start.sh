14739a8d40b6d1ff22929d95359047aa681c320d
nikrow <nicolasjosea@gmail.com>
2025-06-26 10:31:42 -0400

    Start

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 php artisan cache:clear
-composer dump-autoloader
+composer dump-autoload
 php artisan migrate --force
 php artisan config:cache
 php artisan event:cache
 php artisan route:cache
-php -d memory_limit=256M artisan view:cache
-php -d memory_limit=256M artisan optimize
-php artisan filament:optimize
+#php -d memory_limit=256M artisan view:cache
+#php -d memory_limit=256M artisan optimize
+#php artisan filament:optimize
 php artisan octane:frankenphp --host=0.0.0.0 --port=${PORT:-8080}
